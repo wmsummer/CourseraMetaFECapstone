c20a985c84638f365508d6a6f8a8714c3cee07bb
wmsummer <34860410+wmsummer@users.noreply.github.com>
2023-11-27 07:03:33 -0500

    replaced div with fragment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import {Fragment} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <Fragment className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </Fragment>
   );
 }
 
